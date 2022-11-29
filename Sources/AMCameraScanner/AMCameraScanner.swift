10acbd6a9f4e07c2bc8635a666b3d9fb78542572
Evgenij Romanishyn <evgenijromanishyn@gmail.com>
2022-11-29 09:35:44 +0200

    update

--- Sources/AMCameraScanner/AMCameraScanner.swift
+++ Sources/AMCameraScanner/AMCameraScanner.swift
@@ -67,13 +67,11 @@ public class AMCameraScanner: UIViewController, AVCaptureVideoDataOutputSampleBu
     }
 
     // MARK: Inits
-    init() {
+    public init() {
         super.init(nibName: nil, bundle: nil)
     }
 
-    required init?(
-        coder: NSCoder
-    ) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
